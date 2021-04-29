3b22ca5d363d83f3bb6c5acbacb158232e634824
8zca <excref@gmail.com>
2021-04-30 00:54:20 +0900

    [Update] 不要コード削除

--- app/api/v1/todos.py
+++ app/api/v1/todos.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 from app.domain.model.todo.todo_repository import TodoRepository
-from app.gateway import db
-from app.domain.model.todo.todo import Todo
+
 
 router = APIRouter()
-
 
 @router.get("/")
 def read_items():
